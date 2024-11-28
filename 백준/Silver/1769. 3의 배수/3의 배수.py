811636b77012983db2f8f756cc82f8f18189cfdd
kimmikmikimmim <67899735+uj-kim@users.noreply.github.com>
2024-11-28 19:14:43 +0900

    [Silver V] Title: 3의 배수, Time: 128 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Silver/1769. 3의 배수/3의 배수.py
+++ 백준/Silver/1769. 3의 배수/3의 배수.py
@@ -1,10 +1,10 @@
 def solve():
-    X = input().strip()  # 입력된 숫자를 문자열로 처리
+    X = input()  # 입력된 숫자를 문자열로 처리
     count = 0  # 변환 횟수
 
     # 자릿수의 합이 한 자리수가 될 때까지 반복
-    while len(X) > 1:
-        X = str(sum(map(int, X)))  # 자릿수 합 계산
+    while len(str(X)) > 1:
+        X = sum(map(int, str(X))) # 자릿수 합 계산
         count += 1  # 변환 횟수 증가
 
     # 결과 출력
